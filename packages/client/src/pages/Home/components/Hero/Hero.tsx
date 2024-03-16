7807c18361e09a71d182e4de8da44d932bfe69df
l.bekirova <l.bekirova@galaksion.com>
2024-03-16 18:49:46 +0300

    GCC-29 game start page

--- packages/client/src/pages/Home/components/Hero/Hero.tsx
+++ packages/client/src/pages/Home/components/Hero/Hero.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import CrazyTitle from '@components/CrazyTitle'
 import style from './hero.module.css'
 import PlayButton from '@components/PlayButton'
+import { Link } from 'react-router-dom'
+import { Routes } from '@routes/constants'
 
 const Hero = () => {
   return (
     <div className={style.containerHero}>
       <CrazyTitle>jump-jump</CrazyTitle>
-      <PlayButton />
+      <Link to={Routes.Game}>
+        <PlayButton />
+      </Link>
     </div>
   )
 }
