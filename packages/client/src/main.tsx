861087d113618082f25d3bba50b130b6f0554728
Anastasia Kurochkina <kuronastya14@yandex.ru>
2024-03-24 21:33:37 +0400

    test

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(registration => {
+                console.log('Service worker registered: ', registration)
+            })
+            .catch(error => {
+                console.error('Service worker registration failed: ', error)
+            })
+    })
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
