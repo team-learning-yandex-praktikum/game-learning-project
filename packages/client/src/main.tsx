0c6b48b873a78ec57fcb452e6d0e00329444325a
Anastasia Kurochkina <kuronastya14@yandex.ru>
2024-03-24 21:33:37 +0400

    test

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,6 +5,19 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from '@store'
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(registration => {
+                console.log('Service worker registered: ', registration)
+            })
+            .catch(error => {
+                console.error('Service worker registration failed: ', error)
+            })
+    })
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <BrowserRouter>
