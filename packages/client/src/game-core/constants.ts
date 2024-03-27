1cbd7752866d82865091ed7c2617241cf3dc4709
sivolov.artur <sivolov.artur@otr.ru>
2024-03-27 19:20:11 +0300

    реализация механики "двойной прыжок"

--- packages/client/src/game-core/constants.ts
+++ packages/client/src/game-core/constants.ts
@@ -1,5 +1,5 @@
 export const CanvasWidth = 500
-export const CanvasHeight = window.innerHeight - 50
+export const CanvasHeight = window.innerHeight - 150
 
 export const MsInSec = 1000
 export const ImgDir = '../assets/images/'
