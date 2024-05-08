99bc0485c5cfd2a996ebee6f4c9a80c5515e69cb
l.bekirova <l.bekirova@galaksion.com>
2024-05-08 15:28:21 +0300

    GCC-56 добавлено Web Audio API, звук при прыжке

--- packages/client/src/game-core/input/InputHandler.ts
+++ packages/client/src/game-core/input/InputHandler.ts
@@ -3,21 +3,29 @@ import { Keys, getKeyByCode } from './utils'
 
 export class InputHandler {
     private pressedKeys: Indexed<boolean> = {}
+    private audioContext: AudioContext
+    private jumpOscillator: OscillatorNode | null = null
+    private jumpGainNode: GainNode | null = null
 
     constructor() {
         document.addEventListener('keydown', e => {
             this.setKey(e, true)
+            this.handleJumpSound(e)
         })
 
         document.addEventListener('keyup', e => {
             this.setKey(e, false)
+            this.handleJumpSound(e)
         })
 
         if (typeof window !== 'undefined') {
             window.addEventListener('blur', e => {
                 this.pressedKeys = {}
+                this.stopJumpSound()
             })
         }
+
+        this.audioContext = new window.AudioContext()
     }
 
     private setKey(e: KeyboardEvent, pressed: boolean) {
@@ -66,5 +74,41 @@ export class InputHandler {
 
     get releasedSpace() {
         return this.isUp(Keys.SPACE)
+    }
+
+    handleJumpSound(e: KeyboardEvent) {
+        if (e.code === 'Space' && e.type === 'keydown') {
+            this.playJumpSound()
+        } else if (e.code === 'Space' && e.type === 'keyup') {
+            this.stopJumpSound()
+        }
+    }
+
+    private playJumpSound() {
+        const oscillator = this.audioContext.createOscillator()
+        const gainNode = this.audioContext.createGain()
+
+        oscillator.connect(gainNode)
+        gainNode.connect(this.audioContext.destination)
+
+        // Настройки звука (частота)
+        oscillator.frequency.setValueAtTime(
+            261.6,
+            this.audioContext.currentTime
+        )
+        // Настроки громкомти
+        gainNode.gain.setValueAtTime(0.1, this.audioContext.currentTime)
+
+        oscillator.start()
+        this.jumpOscillator = oscillator
+        this.jumpGainNode = gainNode
+    }
+
+    private stopJumpSound() {
+        if (this.jumpOscillator && this.jumpGainNode) {
+            this.jumpOscillator.stop()
+            this.jumpOscillator.disconnect()
+            this.jumpGainNode.disconnect()
+        }
     }
 }
